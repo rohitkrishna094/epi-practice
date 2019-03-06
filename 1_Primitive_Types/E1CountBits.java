490e6dc490a31fac27973f6cef23970ca515ca90
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-03-06 10:02:49 -0500

    Added gitignore and countBits set to 1 program

---
+++ 1_Primitive_Types/E1CountBits.java
@@ -0,0 +1,25 @@
+/*
+ *  Write a program to count the number of bits that are set to 1 in an integer.
+ * 
+*/
+public class E1CountBits {
+
+    public static void main(String[] args) {
+        int input = 5;
+        if (args.length == 1) {
+            input = Integer.parseInt(args[0]);
+        }
+        int ans = countBits(input);
+        System.out.println(ans);
+    }
+
+    public static int countBits(int num) {
+        int ans = 0;
+        while (num != 0) {
+            ans += (num & 1);
+            num = num >> 1;
+        }
+        return ans;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# epi-practice" 
+
+
+Note to self:
+FileName Conventions:
+
+E1CountBits.java represents that E is an example, 1 is the number, and the name is the program description.
+There are other types for letters like P for problem(found at back of chapter) and so forth
