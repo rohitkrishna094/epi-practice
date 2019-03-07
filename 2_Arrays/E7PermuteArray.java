cf504c833c1be9968487c7b1be6f71df7e410a20
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-03-07 11:51:55 -0500

    permute array without using extra space: need to be tested since its not tb solution

--- 2_Arrays/E7PermuteArray.java
+++ 2_Arrays/E7PermuteArray.java
@@ -13,16 +13,26 @@ public class E7PermuteArray {
     public static void main(String[] args) {
         // answer should be {20, 30, 10, 40}
         int[] arr = { 10, 20, 30, 40 };
-        int[] perm = { 2, 0, 1, 3 };
+        // int[] perm = { 2, 0, 1, 3 };
+        int[] perm = { 3, 1, 2, 0 };
         int[] result = permute_extraSpace(arr, perm);
         printArray(arr);
         printArray(result);
         System.out.println("-----------------------");
+        permute_efficient(arr, perm);
+        printArray(arr);
 
     }
 
+    /*
+     * Time Complexity is O(n). Not given in the textbook, not sure if this is 100%
+     * right.
+     */
     public static void permute_efficient(int[] arr, int[] perm) {
-
+        for (int i = 0; i < perm.length; i++) {
+            // elemtn i should go to perm[i]
+            swap(arr, i, perm[i]);
+        }
     }
 
     /*
@@ -41,6 +51,12 @@ public class E7PermuteArray {
             System.out.print(a[i] + ", ");
         }
         System.out.println();
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
