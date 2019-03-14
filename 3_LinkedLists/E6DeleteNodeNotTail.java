37a3f3d52017e0305b985eef0f400165a30b5e19
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-03-14 09:48:07 -0400

    delete node in ll as long as its not the last element

---
+++ 3_LinkedLists/E6DeleteNodeNotTail.java
@@ -0,0 +1,46 @@
+/*
+ * Write a program which deletes a node in Linked List. Input node will not be tail node.
+*/
+public class E6DeleteNodeNotTail {
+    public static void main(String[] args) {
+        // this wont work, but look at delete method to understand the code
+    }
+
+    // Assume that the parameter is exactly the node to delete and that it is not
+    // the last element or tail in linkedlist
+    public static delete(ListNode nodeToDelete) {
+        // copy next node's data into this node
+        nodeToDelete.data = nodeToDelete.next.data;
+        nodeToDelete.next = nodeToDelete.next.next;
+    }
+
+    public static void printLL(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.data + " -> ");
+            cur = cur.next;
+        }
+        System.out.println("null");
+    }
+
+    public static ListNode constructLL(int[] a) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        for (int i = 0; i < a.length; i++) {
+            dummy.next = new ListNode(a[i]);
+            dummy = dummy.next;
+        }
+        return head.next;
+    }
+
+    static class ListNode {
+        public int data;
+        public ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            next = null;
+        }
+
+    }
+}
